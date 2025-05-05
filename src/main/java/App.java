2c058b91f5af4ff7bc98f21669afae179b4c16e8
Adam Bonde <adam.bonde@gmail.com>
2025-05-05 13:23:24 +0200

    Readme och ändrat version i pom.xml

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,28 +8,26 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
- 
-public class App extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-  
-  
-  Parent root;
-  try {
-    root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
-    Scene scene = new Scene(root);
 
-    primaryStage.setTitle("Wordle");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-  } catch (IOException e) {
-    e.printStackTrace();
+public class App extends Application {
+  @Override
+  public void start(Stage primaryStage) {
+
+    Parent root;
+    try {
+      root = FXMLLoader.load(getClass().getResource("/MainScene.fxml"));
+      Scene scene = new Scene(root);
+
+      primaryStage.setTitle("Wordle");
+      primaryStage.setScene(scene);
+      primaryStage.show();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
   }
 
-
-    }
- 
- public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
